277f5aa222957db02484ee5318083aa251934ce3
Manuela Garcia <mgh9517@gmail.com>
2025-07-28 10:16:12 -0500

    Update main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap () {
+  const port = process.env.PORT || 5002
+  const host = process.env.HOST || '0.0.0.0'
+
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  try {
+    await app.listen(port, host, () => {
+      console.log(`Server running on ${port}`)
+    })
+  } catch (e) {
+    console.error('Error status: ', e.status)
+    console.error('Error message: ', e.message)
+    process.exit(1)
+  }
 }
 bootstrap()
